71c433bb5203fe32ad335eb56da99dbcd1312cad
Khalid <boulkrim.khalid@protonmail.com>
2025-02-06 08:44:19 +0100

    ex02

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -3,12 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kboulkri <kboulkri@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 22:14:49 by kboulkri          #+#    #+#             */
-/*   Updated: 2025/02/05 22:17:16 by kboulkri         ###   ########.fr       */
+/*   Updated: 2025/02/06 08:43:02 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <iostream>
+#include <exception>
+#include <ctime>
+#include <cstdlib>
+#include <string>
+
+#define RESET   "\033[0m"
+#define RED	 "\033[31m"
+#define BLUE	"\033[34m" 
+#define YELLOW  "\033[33m"
+#define GREEN   "\033[32m"
+
+template <typename T>
 
 class Array
 {
@@ -16,8 +30,106 @@ class Array
 
     Array();
     Array(unsigned int nb);
-    
+    Array(const Array &src);
+    Array & operator=(const Array &rhs);
+    T &operator[](unsigned int nb);
     ~Array();
+
+    unsigned int size() const;
+    T getValue(unsigned int i) const;
+    void setValue(unsigned int i, T value);
+
+    class OutofBound : public std::exception
+    {
+        public :
+        
+            virtual const char *what() const throw()
+            {
+                return ("Out of Bounds");
+            }
+    };
     
     private :  
+
+        T *_array;
+        unsigned int _size;
 };
+
+template <typename T>
+
+Array<T>::Array() : _array(new T[0]), _size(0)
+{
+    std::cout << BLUE << "Array constructor by default" << RESET << std::endl;
+}
+
+template <typename T>
+
+Array<T>::Array(const Array &src) : _size(src._size), _array(new T[this->size])
+{
+    for(unsigned int i = 0; i < this->_size; i++)
+    {
+        this->_array[i] = src._array[i];
+    }
+}
+
+template <typename T>
+
+Array<T>::Array(unsigned int nb) : _array(new T[nb]), _size(nb)
+{
+    std::cout << BLUE <<"Array constructor unsigned int default" << RESET << std::endl;
+}
+template <typename T>
+
+Array<T>::~Array()
+{
+    delete [] this->_array;
+}
+
+template <typename T>
+
+Array<T> & Array<T>::operator=(const Array &rhs)
+{
+    if (*this != &rhs)
+    {
+        delete[] this->_array;
+        this->_size = rhs._size;
+        this->_array = new T[this->_size];
+        for (unsigned int i = 0; i < this->_size; i++)
+        {
+            this->_array[i] = rhs._array[i];
+        }
+    }
+    return *this;
+}
+
+template <typename T>
+
+T & Array<T>::operator[](unsigned int nb)
+{
+    if (nb > this->_size || nb < 0)
+        throw OutofBound();
+    return (this->_array[nb]);    
+}
+
+template <typename T>
+
+T Array<T>::getValue(unsigned int i) const
+{
+    return (this->_array[i]);
+}
+
+template <typename T>
+
+unsigned int Array<T>::size() const
+{
+    return (this->_size);
+}
+
+template <typename T>
+
+void Array<T>::setValue(unsigned int i, T value)
+{
+    if (i >= _size)
+        throw OutofBound();
+    this->_array[i] = value;
+}
--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -3,10 +3,54 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kboulkri <kboulkri@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 20:04:56 by kboulkri          #+#    #+#             */
-/*   Updated: 2025/02/05 20:05:13 by kboulkri         ###   ########.fr       */
+/*   Updated: 2025/02/06 08:43:09 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Array.hpp"
+
+
+
+int main()
+{
+    std::cout << RED << "Array of int with a size of 5" << RESET << std::endl;
+    Array<int> a(5);
+    srand(time(NULL));
+    for(unsigned int i = 0; i < 5; i++)
+    {
+        a.setValue(i, rand() % 3);   
+        std::cout << YELLOW << "Random value from Int array[" << i << "] -> " << a.getValue(i) << RESET << std::endl;
+    }
+
+    std::cout << std::endl << RED << "Array of float with a size of 3" << std::endl;
+    Array<float> b(3);
+    b.setValue(0, 1.0);
+    b.setValue(1, 42.0); 
+    b.setValue(2, 4.2);
+    for(unsigned int i = 0; i < 3; i++)
+    {
+        std::cout << YELLOW << "Value from float array[" << i << "] -> " << b.getValue(i) << RESET << std::endl;
+    }
+    
+    std::cout << std::endl << RED << "Array of string with a size of 3 + OutofBound" << std::endl;
+    Array<std::string> c(3);
+    try
+    {
+        c.setValue(0, "Dans");
+        c.setValue(1, "la");
+        c.setValue(2, "Zone");
+        for (unsigned int i = 0; i < 3; i++)
+        {
+            std::cout << YELLOW << "Value from string array[" << i << "] -> " << c.getValue(i) << RESET << std::endl;
+        }
+        std::cout << RED << "Now trying to create a string that is out of bound" << RESET << std::endl;
+        c.setValue(3, "OUT OF BOUND");
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+}
